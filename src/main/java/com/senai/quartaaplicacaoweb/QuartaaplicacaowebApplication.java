384fb71ddf9b570a7fd0e08e31e7d60fe382aca2
ead <victorhsouzaa@gmail.com>
2023-07-06 10:19:34 -0300

    Adicionando configuração no metodo principal

--- src/main/java/com/senai/quartaaplicacaoweb/QuartaaplicacaowebApplication.java
+++ src/main/java/com/senai/quartaaplicacaoweb/QuartaaplicacaowebApplication.java
@@ -2,8 +2,11 @@ package com.senai.quartaaplicacaoweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+import java.util.zip.DataFormatException;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class QuartaaplicacaowebApplication {
 
 	public static void main(String[] args) {
