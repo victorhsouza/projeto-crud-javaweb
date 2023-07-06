184c3766a7055f7369a7b293f52d21b9cc3fa558
ead <victorhsouzaa@gmail.com>
2023-07-06 11:43:02 -0300

    Mundando dependencia my sql connector

--- src/main/java/com/senai/quartaaplicacaoweb/services/LoginService.java
+++ src/main/java/com/senai/quartaaplicacaoweb/services/LoginService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class LoginService {
 
     @Autowired(required = true)
-    private UserRepositorie userRepositorie;
+     UserRepositorie userRepositorie;
 
 
 
