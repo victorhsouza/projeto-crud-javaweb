65188277d1f48de349af4e600e1993ab2f85ce62
ead <victorhsouzaa@gmail.com>
2023-07-06 10:59:06 -0300

    Adicionando anotações no repositorio

--- src/main/java/com/senai/quartaaplicacaoweb/repositories/UserRepositorie.java
+++ src/main/java/com/senai/quartaaplicacaoweb/repositories/UserRepositorie.java
@@ -3,7 +3,11 @@ package com.senai.quartaaplicacaoweb.repositories;
 import com.senai.quartaaplicacaoweb.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
+@EnableJpaRepositories
+@Repository
 public interface UserRepositorie extends JpaRepository<UserModel,Integer> {
 
 //   Esta é query que sera executada toda vez que o usuario for fazer login; Ela vai verificar na tabela de usuarios se o login e a senha informados existem
